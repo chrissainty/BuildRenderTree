8bf0c42943fc8c7340ebc5fdbc1b7736d8cb077b
Derek Chasse <Derek.Chasse@gmail.com>
2019-04-30 12:13:02 -0400

    TestComponent demonstrating BuildRenderTree issue.

---
+++ Pages/BaseIndex.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.RenderTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildRenderTree.Pages
+{
+    public class BaseIndex : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            Console.WriteLine($"Before BuildRenderTree");
+
+            base.BuildRenderTree(builder);
+            Console.WriteLine($"After BuildRenderTree");
+        }
+
+    }
+}
