ebb73284ffcae76a46becbf7b3e54dab75a7aea6
Chris Sainty <chrissainty@users.noreply.github.com>
2019-04-30 19:11:22 +0100

    Examples

---
+++ Shared/TestComponentExample2.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BuildRenderTree.Shared
+{
+    public class TestComponentExample2Base : ComponentBase
+    {
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+    }
+}
